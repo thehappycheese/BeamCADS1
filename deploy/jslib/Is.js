23b9ffdc979016b1e786e713c1233deb4519476b
thehappycheese <the.nicholas.archer@gmail.com>
2014-03-29 15:30:10 +0800

    2hr correction widget idea

--- deploy/jslib/Is.js
+++ deploy/jslib/Is.js
@@ -1,40 +1,35 @@
-Is = (function(){
+var Is = function(val){
+		var exp = new (function(val){
+			this.v = val;
+			this.numeric	=	function(){
+				return this.v != undefined && !isNaN(this.v) && typeof this.v === "number";
+			}.bind(this)
+			
+			this.notnumeric	=	function(){
+				return !this.numeric(this.v);
+			}.bind(this)
+			
+			this.divisible	=	function(b){
+				return this.numeric() && Is(b).numeric() && (b%this.v === 0);
+			}.bind(this)
+			this.notdivisible = function(b){
+				return !this.divisible(b);
+			}.bind(this);
+			this.indivisible = this.notdivisible;
 
-		numeric:function(v){
-			return v != undefined && !isNaN(v) && typeof v === "number";
-		}.bind(this),
-		
-		notnumeric:function(v){
-			return !this.numeric(v);
-		}.bind(this),
-		
-		A_factorof_B:function(a,b){
-			if(this.notnumeric(a) || this.notnumeric(b)){
-				throw new Error("cannot factor non-numeric values")
+			this.nonzero	=	function(){
+				return this.numeric(this.v) && this.v !== 0;
+			}.bind(this)
+			
+			this.positive	=	function(){
+					return this.numeric() && this.v>0;
+			}.bind(this)
+			this.negative	=	function(){
+					return this.numeric() && this.v<0;
 			}
-			return (b%a === 0);
-		}.bind(this),
-
-		nonzero:function(v){
-			if(this.numeric(v)){
-				return this.numeric(v) && v !== 0;
-			}else{
-				throw new Error("Cant check nonzero on non-numeric.");
-			}
-		}.bind(this),
-		
-		positive:function(v){
-			if(this.numeric(v)){
-				return v>0;
-			}else{
-				throw new Error("Cant check non-numeric is positive.");
-			}
-		},
-		negative:function(v){
-			if(this.numeric(v)){
-				return v<0;
-			}else{
-				throw new Error("Cant check non-numeric is negative.");
-			}
-		},
-	})();
+			this.between = function(v, a,b){
+				return this.numeric() && Is(a).numeric() && Is(b).numeric() && this.v>a && this.v<b;
+			}.bind(this);
+		})(val);
+		return exp;
+	}
